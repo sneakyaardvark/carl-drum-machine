c70429c7f42e45a110b26927c7f679d1644fb250
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-10-23 17:31:53 -0700

    feat: flake.nix devshell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          platformio
+          doxygen
+          clang-tools
+        ];
+        shellHook = ''
+          export PLATFORMIO_CORE_DIR=$PWD/.platformio
+        '';
+      };
+    }
+  );
+}
