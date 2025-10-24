6094364454b8647a0bbaffbd03005740a61074a3
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-10-24 15:32:47 -0700

    fix: use custom beat_pattern type instead of uint32

--- lib/BeatPattern/src/beat_pattern.cpp
+++ lib/BeatPattern/src/beat_pattern.cpp
@@ -5,8 +5,8 @@ static int rev_g(int);
 static int g(int);
 
 beat_pattern cycle_beat(beat_pattern beat, uint8_t position) {
-  uint32_t mask = 3 << position; // 0b11 in the position of the note
-  uint32_t newbeat = (beat & mask) >> position; // the current value as LSBs
+  beat_pattern mask = 3 << position; // 0b11 in the position of the note
+  beat_pattern newbeat = (beat & mask) >> position; // the current value as LSBs
   newbeat = rev_g(newbeat);
   ++newbeat %= 4; // incr  & restrict to 0-3
   return g(newbeat); // return after conversion back to graycode
