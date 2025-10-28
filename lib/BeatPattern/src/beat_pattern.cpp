f3a0390ffbc7da2be8400a84ba2b6e363230332d
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-10-28 12:32:22 -0700

    refactor: better names for gray code funcs

--- lib/BeatPattern/src/beat_pattern.cpp
+++ lib/BeatPattern/src/beat_pattern.cpp
@@ -1,23 +1,21 @@
 #include "beat_pattern.h"
 #include <stdint.h>
 
-static int rev_g(int);
-static int g(int);
+static int fromGrayCode(int);
+static int toGrayCode(int);
 
 beat_pattern cycle_beat(beat_pattern beat, uint8_t position) {
   beat_pattern mask = 3 << position; // 0b11 in the position of the note
   beat_pattern newbeat = (beat & mask) >> position; // the current value as LSBs
-  newbeat = rev_g(newbeat);
-  ++newbeat %= 4; // incr  & restrict to 0-3
-  return g(newbeat); // return after conversion back to graycode
+  newbeat = fromGrayCode(newbeat);
+  ++newbeat %= 4;    // incr  & restrict to 0-3
+  return toGrayCode(newbeat); // return after conversion back to graycode
 }
 
 // to graycode
-static int g(int n) {
-  return n ^ (n >> 1);
-}
+static int toGrayCode(int n) { return n ^ (n >> 1); }
 // from graycode
-static int rev_g (int g) {
+static int fromGrayCode(int g) {
   int n = 0;
   for (; g; g >>= 1)
     n ^= g;
