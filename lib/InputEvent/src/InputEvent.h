f9b4fe7c9160557d0fd6b64822c4f8907cf1f5cd
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-10-28 15:45:20 -0700

    feat: input event abstraction

---
+++ lib/InputEvent/src/InputEvent.h
@@ -0,0 +1,19 @@
+#ifndef H_CARL_INPUTEVENT
+#define H_CARL_INPUTEVENT
+#include <stdint.h>
+
+/// The type of the input
+enum InputType {
+  InputBeatButton = 0, ///< one of the main 8 pattern buttons
+  InputMove = 1, ///< the "arrow" keys
+  InputMod = 2, ///< the modifier key
+};
+
+typedef struct input_event {
+  InputType type;
+  uint32_t data;
+} InputEvent;
+
+void onInputEvent(InputEvent event);
+
+#endif // !H_CARL_INPUTEVENT
