1ee772537f38c6958c4686828bc6dfa6631a050a
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-10-28 12:46:11 -0700

    refactor: use uint8_t over byte

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <beat_pattern.h>
 
 struct carl {
-  byte current_voice = 0;
+  uint8_t current_voice = 0;
   beat_pattern pattern_mtx[NUM_VOICES][NUM_PATTERNS];
   uint8_t tempo = DEFAULT_TEMPO;
 };
