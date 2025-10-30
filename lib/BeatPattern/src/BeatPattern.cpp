d64c2cfdc9bb566ae06130499663794767fbe809
Ethan Budde <61395479+EthanBudde@users.noreply.github.com>
2025-10-30 15:30:55 -0700

    fixed bit movement logic

--- lib/BeatPattern/src/BeatPattern.cpp
+++ lib/BeatPattern/src/BeatPattern.cpp
@@ -3,16 +3,16 @@
 
 
 void BeatPattern::cycleBeat(beat_pattern* beat, uint8_t position) {
-  beat_pattern mask = 3 << position; // 0b11 in the position of the note
-  beat_pattern newbeat = (*beat & mask) >> position; // the current value as LSBs
+  beat_pattern mask = 3 << (2 * position); // 0b11 in the position of the note
+  beat_pattern newbeat = (*beat & mask) >> (2 * position); // the current value as LSBs
   ++newbeat %= 4;    // incr  & restrict to 0-3
-  *beat &= newbeat << position;
+  *beat &= newbeat << (2 * position);
   // call BeatChanged(position)
 }
 
 void BeatPattern::clear() {
   beat = 0;
-  // TODO: trigger cleared event to reset all LEDs
+  // call ClearPattern(patternID)
 }
 
 beat_pattern BeatPattern::get() {
