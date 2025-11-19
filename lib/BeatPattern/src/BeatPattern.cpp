1bf375c85e3637412fe87cfd2f2d5949d8fe4ae7
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-11-18 16:54:22 -0800

    refactor: beat pattern improvements

--- lib/BeatPattern/src/BeatPattern.cpp
+++ lib/BeatPattern/src/BeatPattern.cpp
@@ -1,18 +1,27 @@
 #include "BeatPattern.hpp"
 #include <stdint.h>
 
+/*
+ * set bit(s) n of x to beat
+ */
+inline beat_pattern bit_set(beat_pattern beat, uint8_t n, beat_pattern x) {
+  return (beat & ~((uint8_t)3 << n)) | (x << n);
+}
 
-void BeatPattern::cycleBeat(beat_pattern* beat, uint8_t position) {
-  beat_pattern mask = 3 << (2 * position); // 0b11 in the position of the note
-  beat_pattern newbeat = (*beat & mask) >> (2 * position); // the current value as LSBs
+BeatPattern::BeatPattern() { }
+
+void BeatPattern::cycleBeat(uint8_t position) {
+  beat_pattern mask = 3 << position * 2; // 0b11 in the position of the note
+  beat_pattern newbeat = (beat & mask) >> position * 2; // the current value as LSBs
   ++newbeat %= 4;    // incr  & restrict to 0-3
-  *beat &= newbeat << (2 * position);
+  beat = bit_set(beat, position * 2, newbeat);
+  
   // call BeatChanged(position)
 }
 
 void BeatPattern::clear() {
   beat = 0;
-  // call ClearPattern(patternID)
+  // TODO: trigger cleared event to reset all LEDs
 }
 
 beat_pattern BeatPattern::get() {
