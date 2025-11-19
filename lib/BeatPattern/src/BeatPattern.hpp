8e38ab8838546406a1d09bbb2197d60db0e0c870
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-11-18 17:00:07 -0800

    docs: add doc comments to BeatPattern

--- lib/BeatPattern/src/BeatPattern.hpp
+++ lib/BeatPattern/src/BeatPattern.hpp
@@ -1,7 +1,14 @@
+/**
+ * @file BeatPattern.hpp
+ * @brief An object for manipulating the beat patterns
+ */
 #ifndef H_CARL_BEAT_PATTERN
 #define H_CARL_BEAT_PATTERN
 #include <stdint.h>
 
+/**
+ * @brief the beat pattern type
+ */
 typedef uint16_t beat_pattern;
 
 class BeatPattern {
@@ -10,9 +17,21 @@ class BeatPattern {
 
   public:
     BeatPattern();
+    /**
+     * @brief cycle a beat
+     * @param position the beat to cycle
+     */
     void cycleBeat(uint8_t position);
-    void clear();
-    beat_pattern get();
+    /**
+     * @brief reset this pattern (set all to 0)
+     */
+    void clear(void);
+    /**
+     * @brief get the underlying beat pattern
+     * Note! Use cycleBeat() for changing, this returns a value, not a reference!
+     * @return the pattern
+     */
+    beat_pattern get(void);
 };
 
 #endif // !H_CARL_BEAT_PATTERN
