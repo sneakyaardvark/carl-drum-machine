21c7e22c19be22aa8b6a54c71cc0f264060dbc17
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-12-10 16:02:07 -0800

    refactor: simpler beatpattern impl

--- lib/BeatPattern/src/BeatPattern.cpp
+++ lib/BeatPattern/src/BeatPattern.cpp
@@ -1,22 +1,34 @@
 #include "BeatPattern.hpp"
+#include <string.h>
 #include <stdint.h>
 
-BeatPattern::BeatPattern() { }
+BeatPattern::BeatPattern() {
+  memset(beats, 0, 8);
+}
 
-void BeatPattern::cycleBeat(beat_pattern* beat, uint8_t position) {
-  beat_pattern mask = 3 << (2 * position); // 0b11 in the position of the note
-  beat_pattern newbeat = (*beat & mask) >> (2 * position); // the current value as LSBs
-  ++newbeat %= 4;    // incr  & restrict to 0-3
-  *beat &= newbeat << (2 * position);
-  // call BeatChanged(position)
+void BeatPattern::cycleBeat(uint8_t position) {
+  // 0 <= pos < 8
+  ++beats[position] %= 4;
+  onBeatChangedCallback(position, beats[position]);
+}
+
+bool BeatPattern::isBeatOn(uint8_t position) {
+  // 0 <= pos < 16
+  uint8_t qn_pos = position / 2; // get the quarter note pos, i.e. map 0,1->0, 2,3->1, etc.
+  return beats[qn_pos] == (qn_pos % 2) + 1;
 }
 
 void BeatPattern::clear() {
-  beat = 0;
-  // call ClearPattern(patternID)
+  for (int i = 0; i < 8; i++) {
+    beats[i] = 0;
+    onBeatChangedCallback(i, 0);
+  }
 }
 
 beat_pattern BeatPattern::get() {
-  return beat;
+  return beats;
 }
 
+void BeatPattern::setOnBeatChanged(void (*callback)(uint8_t position, uint8_t state)) {
+  onBeatChangedCallback = callback;
+}
